776ebf1a4ba40b1495c895527f4aefda8e10f717
Byczu95 <lukasz.knop95@wp.pl>
2016-04-16 03:13:48 +0200

    Wysyłanie danych do VCP v0.1
    Wysyłanie danych do VCP

--- PTM_PCINFO/PTM_PCINFO/Program.cs
+++ PTM_PCINFO/PTM_PCINFO/Program.cs
@@ -1,23 +1,47 @@
 ﻿using System;
+using System.IO.Ports;
 using OpenHardwareMonitor.Hardware;
+using System.Threading;
+
 
 
 namespace PTM_PCINFO
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             SystemInfo info = new SystemInfo();
             info.getInfo();
             Console.ReadLine();
         }
+
     }
+
 
     public class SystemInfo
     {
+        static bool _continue;
+        static SerialPort _serialPort = new SerialPort("COM6",
+            9600, Parity.None, 8, StopBits.One);
+
         public void getInfo()
         {
+            Console.WriteLine("Available Ports:");
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                Console.WriteLine("   {0}", s);
+            }
+
+            Thread readThread = new Thread(Read);
+            _serialPort.ReadTimeout = 500;
+            _serialPort.WriteTimeout = 500;
+            _serialPort.Open();
+            _continue = true;
+            readThread.Start();
+
+            
             Computer computer = new Computer()
             {
                 CPUEnabled = true,
@@ -29,86 +53,129 @@ namespace PTM_PCINFO
 
             };
             computer.Open();
-            int k = 0;
-            foreach (var hardware in computer.Hardware)
-            {
-                if (hardware.HardwareType == HardwareType.CPU)
-                {
-                    hardware.Update();
-                    Console.WriteLine("Procesor:");
-                    foreach (var sensor in hardware.Sensors)
-                    {
-                        if (sensor.SensorType == SensorType.Temperature)
-                            Console.WriteLine(sensor.Name + ": " + sensor.Value.GetValueOrDefault() + "°C");
-                        if (sensor.SensorType == SensorType.Load)
-                            Console.WriteLine(sensor.Name + ": " + sensor.Value.GetValueOrDefault() + "%");
-                        if (sensor.SensorType == SensorType.Clock)
-                            Console.WriteLine(sensor.Name + ": " + sensor.Value.GetValueOrDefault() + "MHz");
-                    }     
-                }
 
-                foreach (var subhardware in hardware.SubHardware)
+            int k = 0;
+            while (_continue)
+            {
+                foreach (var hardware in computer.Hardware)
                 {
-                    subhardware.Update();
-                    if (subhardware.Sensors.Length > 0)
+                    if (hardware.HardwareType == HardwareType.CPU)
                     {
-                        foreach (var sensor in subhardware.Sensors)
+                        hardware.Update();
+                        //Console.WriteLine("Procesor:");
+                        foreach (var sensor in hardware.Sensors)
                         {
-                            if (sensor.SensorType == SensorType.Fan && sensor.Name.Equals("Fan #1", StringComparison.OrdinalIgnoreCase))
+                            if (sensor.SensorType == SensorType.Temperature)
                             {
-                                Console.WriteLine("Prędkość coolera: " + Convert.ToString((int)(float)sensor.Value) + "RPM\n");
+                                _serialPort.WriteLine(sensor.Name + ": " + sensor.Value.GetValueOrDefault() + "°C");
+                            }
+                            if (sensor.SensorType == SensorType.Load)
+                            {
+                                _serialPort.WriteLine(sensor.Name + ": " + sensor.Value.GetValueOrDefault() + "°%");
+                            }
+                            if (sensor.SensorType == SensorType.Clock)
+                            {
+                                _serialPort.WriteLine(sensor.Name + ": " + sensor.Value.GetValueOrDefault() + "Mhz");
                             }
                         }
                     }
-                }
 
-                if (hardware.HardwareType == HardwareType.GpuNvidia)
-                {
-                    hardware.Update();
-                    Console.WriteLine("\nKarta graficzna:");
-                    foreach (var sensor in hardware.Sensors)
+                    foreach (var subhardware in hardware.SubHardware)
                     {
-                        if (sensor.SensorType == SensorType.Fan)
-                            Console.WriteLine(sensor.Name + ": " + Convert.ToString(sensor.Value) + "RPM");
-                        if (sensor.SensorType == SensorType.Clock)
-                            Console.WriteLine(sensor.Name + ": " + Convert.ToString(sensor.Value) + "MHz");
-                        if (sensor.SensorType == SensorType.Temperature)
-                            Console.WriteLine(sensor.Name + ": " + Convert.ToString(sensor.Value) + "°C");
-                        if (sensor.SensorType == SensorType.Load)
-                            Console.WriteLine(sensor.Name + ": " + Convert.ToString(sensor.Value) + "%");
+                        subhardware.Update();
+                        if (subhardware.Sensors.Length > 0)
+                        {
+                            foreach (var sensor in subhardware.Sensors)
+                            {
+                                if (sensor.SensorType == SensorType.Fan && sensor.Name.Equals("Fan #1", StringComparison.OrdinalIgnoreCase))
+                                {
+                                    _serialPort.WriteLine("Prędkość coolera: " + Convert.ToString((int)(float)sensor.Value) + "RPM\n");
+                                }
+                            }
+                        }
                     }
-                }
 
-                if (hardware.HardwareType == HardwareType.RAM)
-                {
-                    hardware.Update();
-                    Console.WriteLine("\nPamięć RAM:");
-                    foreach (var sensor in hardware.Sensors)
+                    if (hardware.HardwareType == HardwareType.GpuNvidia)
                     {
-                        if (sensor.SensorType == SensorType.Load)
-                            Console.WriteLine(sensor.Name + ": " + Convert.ToString(sensor.Value) + "%");
-                        if (sensor.SensorType == SensorType.Data)
-                            Console.WriteLine(sensor.Name + ": " + Convert.ToString(sensor.Value) + "GB");
-                    }
-                }
+                        hardware.Update();
+                        //Console.WriteLine("\nKarta graficzna:");
+                        foreach (var sensor in hardware.Sensors)
+                        {
+                            if (sensor.SensorType == SensorType.Fan)
+                            {
+                                _serialPort.WriteLine(sensor.Name + ": " + Convert.ToString(sensor.Value) + "RPM");
+                            }
+                            if (sensor.SensorType == SensorType.Clock)
+                            {
+                                _serialPort.WriteLine(sensor.Name + ": " + Convert.ToString(sensor.Value) + "MHz");
+                            }
+                            if (sensor.SensorType == SensorType.Temperature)
+                            {
+                                _serialPort.WriteLine(sensor.Name + ": " + Convert.ToString(sensor.Value) + "°C");
+                            }
+                            if (sensor.SensorType == SensorType.Load)
+                            {
+                                _serialPort.WriteLine(sensor.Name + ": " + Convert.ToString(sensor.Value) + "%");
+                            }
 
-                if (hardware.HardwareType == HardwareType.HDD)
-                {
-                    k++;
-                    hardware.Update();
-                    Console.WriteLine("\nDysk twardy " + k + ":");
-                    foreach (var sensor in hardware.Sensors)
+                        }
+                    }
+
+                    if (hardware.HardwareType == HardwareType.RAM)
                     {
-                        if (sensor.SensorType == SensorType.Load)
-                            Console.WriteLine(sensor.Name + ": " + Convert.ToString(sensor.Value) + "%");
-                        if (sensor.SensorType == SensorType.Temperature)
-                            Console.WriteLine(sensor.Name + ": " + Convert.ToString(sensor.Value) + "°C");
+                        hardware.Update();
+                        //Console.WriteLine("\nPamięć RAM:");
+                        foreach (var sensor in hardware.Sensors)
+                        {
+                            if (sensor.SensorType == SensorType.Load)
+                            {
+                                _serialPort.WriteLine(sensor.Name + ": " + Convert.ToString(sensor.Value) + "%");
+                            }
+                            if (sensor.SensorType == SensorType.Data)
+                            {
+                                _serialPort.WriteLine(sensor.Name + ": " + Convert.ToString(sensor.Value) + "GB");
+                            }
+                        }
                     }
-                }
 
+                    if (hardware.HardwareType == HardwareType.HDD)
+                    {
+                        k++;
+                        hardware.Update();
+                        //Console.WriteLine("\nDysk twardy " + k + ":");
+                        foreach (var sensor in hardware.Sensors)
+                        {
+                            if (sensor.SensorType == SensorType.Load)
+                            {
+                                _serialPort.WriteLine(sensor.Name + ": " + Convert.ToString(sensor.Value) + "%");
+                            }
+                            if (sensor.SensorType == SensorType.Temperature)
+                            {
+                                _serialPort.WriteLine(sensor.Name + ": " + Convert.ToString(sensor.Value) + "°C");
+                            }
+                        }
+                    }
+
+                }
+                readThread.Join();
+            }
+
+            _serialPort.Close();
+        }
+        public static void Read()
+        {
+            while (_continue)
+            {
+                try
+                {
+                    string message = _serialPort.ReadLine();
+                    Console.WriteLine(message);
+                }
+                catch (TimeoutException) { }
             }
         }
 
     }
 }
+
 
